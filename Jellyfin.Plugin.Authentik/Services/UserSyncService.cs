dcb5358a2e43a20b358cb9270bdcaf23b04cb444
Scott Fridlund <scott@fridwin.com>
2026-05-30 02:19:06 +0000

    Adjusting permissions a bit.

--- Jellyfin.Plugin.Authentik/Services/UserSyncService.cs
+++ Jellyfin.Plugin.Authentik/Services/UserSyncService.cs
@@ -64,10 +64,11 @@ public class UserSyncService
         {
             var isAdmin = userInfo.Groups.Contains(config.AdminGroup, StringComparer.OrdinalIgnoreCase);
 
-            user.SetPermission(PermissionKind.IsAdministrator, isAdmin);
             user.SetPermission(PermissionKind.EnableAllFolders, true);
-            user.SetPermission(PermissionKind.EnableContentDeletion, isAdmin);
+
+            user.SetPermission(PermissionKind.IsAdministrator, isAdmin);
             user.SetPermission(PermissionKind.EnableRemoteControlOfOtherUsers, isAdmin);
+            user.SetPermission(PermissionKind.EnableLiveTvManagement, isAdmin);
 
             _logger.LogDebug(
                 "Synced permissions for {Username}: Admin={IsAdmin}",
